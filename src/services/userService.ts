4519e5f44e9a2d0815b006d59f43b806972cdcfe
ducviet5138 <ducviet5138@gmail.com>
2024-01-29 22:55:24 +0700

    OSW-7 feat: Add swagger | style: Run yarn prettier

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,7 +1,7 @@
 import UserRepository from '@repositories/userRepository';
-import BaseResponse from '@/utils/baseResponse'
+import BaseResponse from '@/utils/baseResponse';
 import { STATUS_CODE } from '@/utils/enum';
-import { CreateUser } from '@/interfaces/createUser'
+import { CreateUser } from '@/interfaces/createUser';
 
 class UserService {
   private repository: any;
@@ -14,25 +14,25 @@ class UserService {
       const { username, email } = data;
       const existedUser = await this.repository.getByEntity({ username, email });
       if (existedUser) {
-        return new BaseResponse(STATUS_CODE.CONFLICT, false, "User already exists")
+        return new BaseResponse(STATUS_CODE.CONFLICT, false, 'User already exists');
       }
       const user = await this.repository.add(data);
-      return new BaseResponse(STATUS_CODE.OK, true, "User created successfully", user)
+      return new BaseResponse(STATUS_CODE.OK, true, 'User created successfully', user);
     } catch (err: any) {
-      return new BaseResponse(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message)
+      return new BaseResponse(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message);
     }
   }
 
   async getUserById(id: number) {
-     try {
-       const user = await this.repository.getByEntity({ id });
-        if (!user) {
-          return new BaseResponse(STATUS_CODE.NOT_FOUND, false, "User not found")
-        }
-        return new BaseResponse(STATUS_CODE.OK, true, "User found", user)
-     } catch (err: any) {
-       return new BaseResponse(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message)
-     }
+    try {
+      const user = await this.repository.getByEntity({ id });
+      if (!user) {
+        return new BaseResponse(STATUS_CODE.NOT_FOUND, false, 'User not found');
+      }
+      return new BaseResponse(STATUS_CODE.OK, true, 'User found', user);
+    } catch (err: any) {
+      return new BaseResponse(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message);
+    }
   }
 }
 
