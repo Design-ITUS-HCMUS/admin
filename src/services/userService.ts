80ff06e3c858b6660f0c4900a628c3afad4f3db8
DuyQuan <bddquan@gmail.com>
2024-01-29 14:06:03 +0700

    OSW-17 style: change class name (BaseResponse)

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,5 +1,5 @@
 import UserRepository from '@repositories/userRepository';
-import BaseRespone  from '@/utils/baseRespone'
+import BaseResponse  from '@/utils/baseRespone'
 import { STATUS_CODE } from '@/utils/enum';
 import { CreateUser } from '@/interfaces/createUser'
 
@@ -14,12 +14,12 @@ class UserService {
       const { username, email } = data;
       const existedUser = await this.repository.getByEntity({ username, email });
       if (existedUser) {
-        return new BaseRespone(STATUS_CODE.CONFLICT, false, "User already exists")
+        return new BaseResponse(STATUS_CODE.CONFLICT, false, "User already exists")
       }
       const user = await this.repository.add(data);
-      return new BaseRespone(STATUS_CODE.OK, true, "User created successfully", user)
+      return new BaseResponse(STATUS_CODE.OK, true, "User created successfully", user)
     } catch (err: any) {
-      return new BaseRespone(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message)
+      return new BaseResponse(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message)
     }
   }
 
@@ -27,11 +27,11 @@ class UserService {
      try {
        const user = await this.repository.getByEntity({ id });
         if (!user) {
-          return new BaseRespone(STATUS_CODE.NOT_FOUND, false, "User not found")
+          return new BaseResponse(STATUS_CODE.NOT_FOUND, false, "User not found")
         }
-        return new BaseRespone(STATUS_CODE.OK, true, "User found", user)
+        return new BaseResponse(STATUS_CODE.OK, true, "User found", user)
      } catch (err: any) {
-       return new BaseRespone(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message)
+       return new BaseResponse(STATUS_CODE.INTERNAL_SERVER_ERROR, false, err.message)
      }
   }
 }
