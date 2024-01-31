759b9ffff016915abe1c17244c6de1ac53fee859
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2024-02-01 01:19:00 +0700

    OSW-20 add success page

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,5 +1,59 @@
 'use client';
 
+import { Button, Typography, Paper } from '@mui/material';
+import Link from 'next/link';
+import { useSearchParams } from 'next/navigation';
+import color from '@/libs/ui/color';
+import Image from 'next/image';
+import transactionCompleted from '@/assets/transactionCompleted.gif';
+
+const styles = {
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '20px',
+    padding: '20px',
+    marginTop: '50px',
+    maxWidth: '500px',
+    margin: 'auto',
+  },
+  successText: {
+    color: color.notification.success,
+  },
+};
+
+const translateStatus = (status: any) => {
+  status = status.toLowerCase();
+  const statusTranslations: { [key: string]: string } = {
+    paid: 'đã thanh toán',
+    pending: 'đang chờ thanh toán',
+    processing: 'đang xử lý',
+    canceled: 'đã hủy',
+  };
+
+  return statusTranslations[status] || '';
+};
+
 export default function PaymentSuccess() {
-  return <div>Success</div>;
+  const searchParams = useSearchParams();
+
+  const orderCode = searchParams.get('orderCode');
+  const status = searchParams.get('status');
+
+  return (
+    <Paper sx={styles.paper}>
+      <Image src={transactionCompleted} alt='Transaction Completed' width={300} height={300} layout='responsive' />
+      <Typography variant='h4' gutterBottom style={styles.successText}>
+        Giao dịch {typeof status === 'string' ? `${translateStatus(status)}` : ''}!
+      </Typography>
+      <Typography variant='h6'>Mã đơn hàng: {orderCode}</Typography>
+      <Link href='/payment' passHref>
+        <Button variant='contained' color='primary'>
+          Trở lại trang thanh toán
+        </Button>
+      </Link>
+    </Paper>
+  );
 }
