3b8b14bb2edb4d22b680b011fd5eac918050a5da
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2024-02-06 05:08:08 +0700

    OSW-20 upload submission dialog

--- src/app/contestant/page.tsx
+++ src/app/contestant/page.tsx
@@ -1,15 +1,21 @@
+'use client';
 import React from 'react';
 import { Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
 
 export default function Contestant() {
+  const router = useRouter();
   return (
     <div>
       <h1>Contestant</h1>
-      <Button variant='contained' color='primary'>
-        Primary
+      <Button variant='contained' color='primary' onClick={() => router.push('/contestant/submission')}>
+        Go to Submission Page
       </Button>
-      <Button variant='contained' color='secondary'>
-        Secondary
+      <Button variant='contained' color='secondary' onClick={() => router.push('/contestant/team-management')}>
+        Go to Team Management Page
+      </Button>
+      <Button variant='contained' color='secondary' onClick={() => router.push('/payment')}>
+        Go to Payment Page
       </Button>
     </div>
   );
