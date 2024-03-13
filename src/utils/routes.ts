bc2ddbabe0ca00ee28ae05e3c32d61f4dbcd09d5
DuyQuan <bddquan@gmail.com>
2024-03-13 11:08:15 +0700

    OSW-44 feat: add missing authentication route

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,5 +1,13 @@
 const publicRoutes = ['/api-doc'];
-const authRoutes = ['/sign-in', '/sign-in/forget-password', '/sign-up'];
+const authRoutes = [
+  '/sign-in',
+  '/sign-in/forget-password',
+  '/sign-in/change-password',
+  '/sign-in/forget-password/otp',
+  '/sign-up',
+  '/sign-up/otp',
+  '/sign-up/success',
+];
 const defaultRoute = '/events';
 const resetRoute = '/sign-in';
 const authApiRoutePrefix = '/api/auth';
